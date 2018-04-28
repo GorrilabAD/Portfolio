4bee58c1862e8c5045743bb285947b179d8d1722
anton <anton@vaio.1.192.168.lan.quantum.ru>
2018-04-29 02:47:20 +0300

    first version

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $('a[href^="#"]').bind('click.smoothscroll', function(){
+    var target = $(this).attr('href'),
+        bl_top = $(target).offset().top;
+        $('body, html').animate( { scrollTop: bl_top}, 2000 );
+        return false;
+  });
+
+$( "#menu__items" ).hide();
+  $( "#menu" ).click(function() {
+
+  $( "#menu__items" ).slideToggle( "slow", function() {
+    // Animation complete.
+  });
+
+
+});
+
+
+
+});
