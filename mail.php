e0563682110681f54d4525be1dd3b9ed824a1c9e
Gorrila-Frontend <lobanov.wrestbild57@gmail.com>
2018-09-07 13:24:03 +0200

    2.1

--- mail.php
+++ mail.php
@@ -1,17 +1,40 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$fio = htmlspecialchars($fio);
-$email = htmlspecialchars($email);
-$name = urldecode($name);
-$email = urldecode($email);
-$name = trim($name);
-$email = trim($email);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("wrestbild57@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
- {     echo "сообщение успешно отправлено";
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'lobanov.wrestbild57.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'mmakingsambO'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('lobanov.wrestbild57.ru'); // от кого будет уходить письмо?
+$mail->addAddress('wrestbild57@gmail.com');     // Кому будет уходить письмо
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
 } else {
-    echo "при отправке сообщения возникли ошибки";
-}?>
+    header('location: thank-you.html');
+}
+?>
